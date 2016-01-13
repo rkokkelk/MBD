5277ced2ba3e62ee45a8e03be1e61a87c5edf58a
Karim El Assal <iisys_@hotmail.com>
2016-01-13 17:12:49 +0100

    Update code style

--- source/src/main/java/nl/utwente/zzp/Helper.java
+++ source/src/main/java/nl/utwente/zzp/Helper.java
@@ -8,8 +8,8 @@ import org.json.*;
 public class Helper {
 
   public static final String KEY = "Polarity";
-  public static final SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss Z yyyy");
-  public static final SimpleDateFormat outputter = new SimpleDateFormat("dd/MM/yy");
+  public static final SimpleDateFormat FORMATTER = new SimpleDateFormat("EEE MMM d HH:mm:ss Z yyyy");
+  public static final SimpleDateFormat OUTPUTTER = new SimpleDateFormat("dd/MM/yy");
   
   public static void increaseCounter(JSONObject twitterMessage){
 		/*
@@ -21,59 +21,57 @@ public class Helper {
 
   public static void increaseCounter(JSONObject twitterMessage, int count){
 
-		int curValue = 0;
-		try{
-    	curValue = twitterMessage.getInt(KEY);
-		} catch (JSONException je){
-			curValue = 0;
-		} finally{
-			curValue++;
-		}
+	int curValue = 0;
+	try {
+	  curValue = twitterMessage.getInt(KEY);
+	} catch (JSONException je) {}
+	
+	curValue++;
 
-    // Modifying this method can be used to normalize the counter
-		twitterMessage.put(KEY, curValue);
+	// Modifying this method can be used to normalize the counter
+	twitterMessage.put(KEY, curValue);
   }
   
   public static boolean hasPolarity(JSONObject twitterMessage){
 
-		int curValue = 0;
-		try{
-    	curValue = twitterMessage.getInt(KEY);
-		} catch (JSONException je){}
+	int curValue = 0;
+	try {
+	  curValue = twitterMessage.getInt(KEY);
+	} catch (JSONException je){}
 
-    return curValue > 0 ? true : false;
+    return curValue > 0;
   }
 
-  public static boolean isZZP(int polarity){
-    
+  public static boolean isZZP(int polarity){    
     // Use this method to alter data which is ZZP or not
     // based upon polarity
-    return true;
-
+    return polarity >= 1;
   }
 
   public static Date parseDate(String input){
     try{
-      return formatter.parse(input);
+      return FORMATTER.parse(input);
     } catch (ParseException pe){
-      System.err.println("Cannot parse input: "+input);
+      System.err.println("Cannot parse input: " + input);
     }
     return null;
   } 
 
   public static String formatDate(Date input){
-    return outputter.format(input);
+    return OUTPUTTER.format(input);
   } 
 
   public static Date getLatestDate(Date input1, Date input2){
-    if(input1 == null)
+    if(input1 == null) {
       return input2;
-    else if(input2 == null)
+    } else if (input2 == null) {
       return input1;
-    else
-      if(input1.after(input2))
+    } else {
+      if (input1.after(input2)) {
         return input1;
-      else
+      } else {
         return input2;
+      }
+    }
   } 
 }
